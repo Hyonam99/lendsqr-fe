1076bdb1c8decc1d2356d227e28621692ed3139b
Hyonam99 <victorbassey69@yahoo.com>
2022-10-25 06:13:34 +0100

    refactored the pagination to be more dynamic using useState and react-pagination package

--- src/DataList.jsx
+++ src/DataList.jsx
@@ -3,39 +3,18 @@ import Filter from './image_icons_asset/filter-button.svg'
 import { useState } from "react";
 import { Link } from 'react-router-dom'
 import Menu from './image_icons_asset/pop-up-icon.svg'
-import Prev from './image_icons_asset/prev btn.svg'
-import Next from './image_icons_asset/next btn.svg'
+// import Prev from './image_icons_asset/prev btn.svg'
+// import Next from './image_icons_asset/next btn.svg'
 import User from './image_icons_asset/users.svg'
 import Loan from './image_icons_asset/loans-icon.svg'
 import Loan_s from './image_icons_asset/loans-savings.svg'
+import ReactPaginate from 'react-paginate';
 
 
 function DataList({allblog}){
 
   const userDB = JSON.parse(localStorage.getItem('allUsers'))
-
-  const [count1, setCount1] = useState(0);
-  const [count2, setCount2] = useState(10);
-
-  const nextP = () => {
-    console.log(count2 + ' count 1 is ' + count1)
-    if(count2 >= 100){
-      setCount2(count2)
-    }else {
-      setCount2(count2 + 10)
-      setCount1(count1 + 10)
-    }
-  }
-  const prevP = () => {
-    console.log(count1 + ' count 2 is ' + count2)
-    if(count1 == 0){
-      setCount1(count1)
-    }else {
-      setCount1(count1 - 10)
-      setCount2(count2 - 10)
-    }
-   
-  }
+  const userCount = userDB.length
 
   const changeDate = (CurrentDt) => {
     const options = { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric' };
@@ -52,10 +31,34 @@ function DataList({allblog}){
       return 'Inactive'
      }
   }
-
-
   
-  const userCount = userDB.length
+  const [users, setUsers] = useState(allblog)
+  const [pageNumber, setPageNumber] = useState(0)
+
+  const userPerPage = 10
+  const pageVisited = pageNumber * userPerPage
+  const displayUsers = users.slice(pageVisited, pageVisited + userPerPage).map((blog) => (
+        
+    <tbody key={blog.id}>
+      
+    <tr>
+<td>{blog.orgName} </td>
+<td>{blog.userName} </td>
+<td>{blog.email} </td>
+<td>{blog.phoneNumber} </td>
+<td> {changeDate(blog.createdAt)} </td>
+<td className="userStatus">{changeStatus(blog.lastActiveDate)} </td>       
+<td><Link to="/UserFullDetails"><img src={Menu}/></Link></td>
+</tr>  
+    </tbody>
+    ))
+    
+    const pageCount = Math.ceil(users.length / userPerPage)
+
+    const changePage = ({selected}) => {
+        setPageNumber(selected)
+    }
+
   return(
     <>
        <section className="Dash-preview">
@@ -94,32 +97,31 @@ function DataList({allblog}){
         <th></th>
       </tr>
       </thead>
-      {allblog.map((blog) => (
-        
-      <tbody key={blog.id}>
-        
-      <tr>
-<td>{blog.orgName} </td>
-<td>{blog.userName} </td>
-<td>{blog.email} </td>
-<td>{blog.phoneNumber} </td>
-<td> {changeDate(blog.createdAt)} </td>
-<td className="userStatus">{changeStatus(blog.lastActiveDate)} </td>       
-<td><Link to="/UserFullDetails"><img src={Menu}/></Link></td>
-</tr>  
-      </tbody>
-      )).slice(count1, count2)}
+      {displayUsers}
+      
     </table>
 
    
     </section>  
+
     <div className="pagebreak">
-      <section>showing <span>{ `${count2 - count1}` }</span> out of 100 </section>
-      <section className="pagination-butttons">
-        <i onClick={prevP}><img src={Prev}/> </i>
-        <p> {`${(count1 / 10) + 1}....`} </p>
-        <i onClick={nextP}><img src={Next}/></i>
-        </section>
+
+    <section>showing <span>{ `${pageVisited + userPerPage} entries` }</span> out of 100 </section>
+    <ReactPaginate 
+      breakLabel="..."
+      nextLabel="next >"
+      previousLabel="< prev"
+      pageCount={pageCount}
+      onPageChange={changePage}
+      containerClassName={'paginateButtons'}
+      previousLinkClassName={'prevButton'}
+      nextLinkClassName={'nextButton'}
+      disabledClassName={'disabledButton'}
+      activeClassName={'activeButton'}
+      renderOnZeroPageCount={null}
+      />
+      
+   
       </div> 
 
     </>
@@ -129,3 +131,36 @@ function DataList({allblog}){
 
 export default DataList
 
+
+/**
+ * <section>showing <span>{ `${count2 - count1}` }</span> out of 100 </section>
+      <section className="pagination-butttons">
+        <i onClick={prevP}><img src={Prev}/> </i>
+        <p> {`${(count1 / 10) + 1}....`} </p>
+        <i onClick={nextP}><img src={Next}/></i>
+        </section>
+
+
+        // const [count1, setCount1] = useState(0);
+  // const [count2, setCount2] = useState(10);
+
+  // const nextP = () => {
+  //   console.log(count2 + ' count 1 is ' + count1)
+  //   if(count2 >= 100){
+  //     setCount2(count2)
+  //   }else {
+  //     setCount2(count2 + 10)
+  //     setCount1(count1 + 10)
+  //   }
+  // }
+  // const prevP = () => {
+  //   console.log(count1 + ' count 2 is ' + count2)
+  //   if(count1 == 0){
+  //     setCount1(count1)
+  //   }else {
+  //     setCount1(count1 - 10)
+  //     setCount2(count2 - 10)
+  //   }
+   
+  // }
+ */
